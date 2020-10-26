d9fde36e8cc16c34822b6db8dbac306d02012006
ManuDeveloper <manuepeva@gmail.com>
2020-10-26 10:34:28 -0500

    Initial commit of the redux and react shopping cart project

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../index.css';
+import { css } from '@emotion/core';
+import styled from '@emotion/styled';
+
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            All Rights is Reserved
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/components/Heading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../index.css';
+const Heading = () => {
+    return (
+        <div className="header">
+            <a href="/">React Shopping Cart</a>
+        </div>
+    )
+}
+
+export default Heading
