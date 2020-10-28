3a10e7f7b5380b4956e6ad5b14c91635f059e415
ManuDeveloper <manuepeva@gmail.com>
2020-10-28 09:50:48 -0500

    Checkout Form Implementation and event handlers declarations

--- src/App.js
+++ src/App.js
@@ -12,10 +12,15 @@ class App extends React.Component {
     super();
     this.state = {
       products: data.products,
-      cartItems: [],
+      cartItems: localStorage.getItem("cartItems") ?
+        JSON.parse(localStorage.getItem("cartItems")) : [],
       size: "",
       sort: ""
     }
+  }
+
+  createOrder = (order) => {
+    alert("Need to save order for: " + order.name)
   }
 
   removeFromCart = (product) => {
@@ -23,6 +28,7 @@ class App extends React.Component {
     this.setState({
       cartItems: cartItems.filter(x => x._id !== product._id)
     });
+    localStorage.setItem("cartItems", JSON.stringify(cartItems.filter(x => x._id !== product._id)));
   };
 
   addToCart = (product) => {
@@ -41,6 +47,7 @@ class App extends React.Component {
       });
     }
     this.setState({ cartItems });
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
   }
 
   sortProducts = (event) => {
@@ -103,6 +110,7 @@ class App extends React.Component {
               <Cart
                 cartItems={this.state.cartItems}
                 removeFromCart={this.removeFromCart}
+                createOrder={this.createOrder}
               />
             </div>
 
